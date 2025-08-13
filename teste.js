9738bc83b8c7718274557b93c0d9cf7ad3fff697
Priscilabotini <pri.botini@hotmail.com>
2025-08-13 18:00:12 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,46 @@
+
+
+
+
+ function imc(peso, altura){
+   let imc  = peso/altura**2
+   console.log(imc)
+ }
+
+
+ let peso =  parseFloat(prompt('Peso: '))
+ let altura =  parseFloat(prompt('altura: '))
+ imc(peso,altura)
+
+
+
+ function imc(){
+    let peso =  parseFloat(prompt('Peso: '))
+    let altura =  parseFloat(prompt('altura: '))
+     let imc  = peso/(altura**2)
+    console.log(imc)
+ }
+
+
+ imc()
+
+
+ let peso =  parseFloat(prompt('Peso: '))
+ let altura =  parseFloat(prompt('altura: '))
+
+
+
+  function imc(){
+     let imc  = peso/(altura**2)
+    console.log(imc)
+}
+
+
+
+ imc()
+
+
+
+
+
+
